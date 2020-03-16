b232c7f430cd411a89080d11efebe2e8ac2346ab
Rustam <mirzamorph@gmail.com>
2020-03-17 00:24:21 +0300

    added new image api

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -7,18 +7,23 @@ export default class SwapiService {
   }
 
   _baseUrl = 'https://swapi.co/api/';
+  _baseImgUrl = 'https://starwars-visualguide.com/assets/img/';
   apiData = {
     people: {
       url: 'people/',
+      imgUrl: 'characters/',
       method: this._transformPerson
     }
   };
 
-  _transformPerson(item) {
+  _transformPerson(self, item) {
     const pattern = /\/(\d+)\/$/;
+    const id = item.url.match(pattern)[1];
+    const img = self._baseImgUrl + self.apiData.people.imgUrl + id + '.jpg';
     return {
-      id: item.url.match(pattern)[1],
+      id,
       name: item.name,
+      img,
       height: item.height,
       hairColor: item.hair_color,
       eyeColor: item.eye_color
@@ -29,7 +34,7 @@ export default class SwapiService {
     const { url, method } = data;
     const fetchUrl = this._baseUrl + url;
     const result = await axios.get(fetchUrl);
-    return result.data.results.map(method);
+    return result.data.results.map((item) => method(this, item));
   };
 
 }
