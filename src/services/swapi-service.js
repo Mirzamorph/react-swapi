48ed3cff61f8174c40c65733990029c4fe2abfef
Rustam <mirzamorph@gmail.com>
2020-03-16 20:36:24 +0300

    changed api

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -2,11 +2,34 @@ import axios from 'axios';
 
 export default class SwapiService {
 
-  _baseUrl = 'https://swapi.co/api/';
+  constructor() {
+    this._transformPerson = this._transformPerson.bind(this);
+  }
 
-  fetchData = async (url) => {
+  _baseUrl = 'https://swapi.co/api/';
+  apiData = {
+    people: {
+      url: 'people/',
+      method: this._transformPerson
+    }
+  };
+
+  _transformPerson(item) {
+    const pattern = /\/(\d+)\/$/;
+    return {
+      id: item.url.match(pattern)[1],
+      name: item.name,
+      height: item.height,
+      hairColor: item.hair_color,
+      eyeColor: item.eye_color
+    }
+  };
+
+  fetchData = async (data) => {
+    const { url, method } = data;
     const fetchUrl = this._baseUrl + url;
-    return await axios.get(fetchUrl);
+    const result = await axios.get(fetchUrl);
+    return result.data.results.map(method);
   };
 
 }
