f4a66db3ca659eaac54938df242d2dcc44e5d456
Rustam <mirzamorf@yandex.ru>
2020-04-05 00:15:20 +0300

    refactor with eslint

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,21 +1,19 @@
-import React from "react";
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { HomePage, StarshipsPage, PlanetsPage } from '../pages';
 
-const App = () => {
-  return (
-    <Switch>
-      <Route path="/" exact>
-        <HomePage/>
-      </Route>
-      <Route path="/starships" exact>
-        <StarshipsPage/>
-      </Route>
-      <Route path="/planets" exact>
-        <PlanetsPage/>
-      </Route>
-    </Switch>
-  )
-};
+const App = () => (
+  <Switch>
+    <Route path="/" exact>
+      <HomePage />
+    </Route>
+    <Route path="/starships" exact>
+      <StarshipsPage />
+    </Route>
+    <Route path="/planets" exact>
+      <PlanetsPage />
+    </Route>
+  </Switch>
+);
 
 export default App;
