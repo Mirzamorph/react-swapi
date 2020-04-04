a3e996b6f7aca75f0e3f9fb52af2b4789a139df8
Rustam <mirzamorf@yandex.ru>
2020-04-05 00:15:51 +0300

    refactor with eslint

--- src/components/error-boundary/error-boundary.jsx
+++ src/components/error-boundary/error-boundary.jsx
@@ -2,20 +2,24 @@ import React, { Component } from 'react';
 import ErrorIndicator from '../error-indicator';
 
 export default class ErrorBoundary extends Component {
-
-  state = {
-    hasError: false
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
 
   componentDidCatch() {
     this.setState({ hasError: true });
   }
 
   render() {
-    if (this.state.hasError) {
-      return <ErrorIndicator/>;
+    const { hasError } = this.state;
+    const { children } = this.props;
+    if (hasError) {
+      return <ErrorIndicator />;
     }
 
-    return this.props.children;
+    return children;
   }
 }
