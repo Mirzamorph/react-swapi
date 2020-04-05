662aedf7abfe34dd6e7a390dab54638d63bb7c60
Rustam <mirzamorf@yandex.ru>
2020-04-06 00:49:01 +0300

    refactor error indicator with eslint

--- src/components/error-indicator/error-indicator.jsx
+++ src/components/error-indicator/error-indicator.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 const ErrorIndicator = ({ message }) => {
   return (
     <div className="container">
       <div className="alert alert-danger" role="alert">
-        {
-          message || 'A simple danger alert—check it out!'
-        }
+        {message || 'A simple danger alert—check it out!'}
       </div>
     </div>
-  )
+  );
 };
 
 export default ErrorIndicator;
