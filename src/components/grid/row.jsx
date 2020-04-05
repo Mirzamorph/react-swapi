63c952d5563543482dcca0d1f27a57516d6c1c9d
Rustam <mirzamorf@yandex.ru>
2020-04-05 22:45:11 +0300

    row refactor with eslint

--- src/components/grid/row.jsx
+++ src/components/grid/row.jsx
@@ -1,21 +1,16 @@
-import React from "react";
+import React from 'react';
 
 const Row = ({ left, right, leftSize, rightSize }) => {
-
   const leftConSize = leftSize !== undefined ? `col-${leftSize}` : 'col';
   const rightConSize = rightSize !== undefined ? `col-${rightSize}` : 'col';
   return (
     <div className="container">
       <div className="row">
-        <div className={leftConSize}>
-          {left}
-        </div>
-        <div className={rightConSize}>
-          {right}
-        </div>
+        <div className={leftConSize}>{left}</div>
+        <div className={rightConSize}>{right}</div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Row;
