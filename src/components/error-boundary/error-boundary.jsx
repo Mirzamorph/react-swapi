3584dafd14e510b20295f6a372dd702716e7ccbb
Rustam <mirzamorf@yandex.ru>
2020-04-06 20:58:58 +0300

    refactor component state

--- src/components/error-boundary/error-boundary.jsx
+++ src/components/error-boundary/error-boundary.jsx
@@ -2,12 +2,9 @@ import React, { Component } from 'react';
 import ErrorIndicator from '../error-indicator';
 
 export default class ErrorBoundary extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hasError: false,
-    };
-  }
+  state = {
+    hasError: false,
+  };
 
   componentDidCatch() {
     this.setState({ hasError: true });
